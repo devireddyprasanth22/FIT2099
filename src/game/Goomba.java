bdc426400bfa6cf5df58d1a3d8d29929bdf73790
Jack Draganich <jdra0003@student.monash.edu>
2022-05-02 18:08:43 +1000

    Req 3 draft2: resolved issue with koopa dying by adjusting AttackAction

--- src/game/Goomba.java
+++ src/game/Goomba.java
@@ -28,12 +28,19 @@ public class Goomba extends Actor {
 	 * Constructor.
 	 */
 	public Goomba() {
+		/**
+		 * Constructor
+		 */
 		super("Goomba", 'g', 20);
 		this.behaviours.put(10, new WanderBehaviour());
 
 	}
 
 	public void tick(Location currentLocation){
+		/**
+		 * currentLocation is location of object on the map
+		 *
+		 */
 		int r1 = (int)(Math.random()*(11-1)+1);
 		System.out.println(r1);
 		//suicide mechanic
@@ -55,10 +62,17 @@ public class Goomba extends Actor {
 	}
 
 	public int getHp(){
+		/**
+		 * returns hp of Goomba
+		 */
 		return hp;
 	}
 
 	public boolean isPlayerInAttackRange(Location currentLocation){
+		/**
+		 * currentLocation: location of Goomba on the map
+		 * returns: true or false if Mario is in attack range or not
+		 */
 		int x = currentLocation.x();
 		int y = currentLocation.y();
 		int[] xArr = {-1, 0, 1};
@@ -82,6 +96,10 @@ public class Goomba extends Actor {
 		return false;
 	}
 	public Actor getPlayerObj(Location currentLocation){
+		/**
+		 * currentLocation: current location of Goomba object
+		 * returns: Actor object in range of player. If not in range, return null
+		 */
 		int x = currentLocation.x();
 		int y = currentLocation.y();
 		int[] xArr = {-1, 0, 1};
@@ -107,6 +125,13 @@ public class Goomba extends Actor {
 
 	@Override
 	public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+		/**
+		 * otherActor: Actor around the player
+		 * direction: Direction in which otherActor is relative to Goomba
+		 * map: current GameMap object
+		 *
+		 * returns: An ActionList object of actions that the other player can do to Goomba
+		 */
 		ActionList actions = new ActionList();
 		// it can be attacked only by the HOSTILE opponent, and this action will not attack the HOSTILE enemy back.
 		if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
@@ -121,6 +146,14 @@ public class Goomba extends Actor {
 	 */
 	@Override
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+		/**
+		 * actions: ActionList of allowable actions of Goomba
+		 * lastAction: Previous action of Goomba
+		 * map: current GameMap
+		 * display: current Display object
+		 *
+		 * returns: current action of Goomba. If none, returns DoNothingAction
+		 */
 		Location location = map.locationOf(this);
 		tick(location);
 		for(Behaviour Behaviour : behaviours.values()) {
--- src/game/Koopa.java
+++ src/game/Koopa.java
@@ -20,22 +20,26 @@ public class Koopa extends Actor{
     private int hp;
 
     /**
-     * Constructor.
      *
-     * @param name        the name of the Actor
-     * @param displayChar the character that will represent the Actor in the display
-     * @param hitPoints   the Actor's starting hit points
      */
     public Koopa() {
+        /**
+         * Constructor
+         */
         super("Koopa", 'K', 100);
 
     }
 
     public void tick(Location currentLocation){
+        /**
+         *
+         * currentLocation is location of object on the map
+         *
+         */
         int r1 = (int)(Math.random()*(11-1)+1);
         System.out.println(r1);
         //Dormant mechanic
-        if(this.getHp() < 0){
+        if(this.getHp() <= 0){
             this.setDisplayChar('D');
         }
         //attack mechanic
@@ -53,10 +57,17 @@ public class Koopa extends Actor{
     }
 
     public int getHp(){
+        /**
+         * returns hp of Koopa
+         */
         return hp;
     }
 
     public boolean isPlayerInAttackRange(Location currentLocation){
+        /**
+         * currentLocation: location of Koopa on the map
+         * returns: true or false if Mario is in attack range or not
+         */
         int x = currentLocation.x();
         int y = currentLocation.y();
         int[] xArr = {-1, 0, 1};
@@ -80,6 +91,10 @@ public class Koopa extends Actor{
         return false;
     }
     public Actor getPlayerObj(Location currentLocation){
+        /**
+         * currentLocation: current location of Koopa object
+         * returns: Actor object in range of player. If not in range, return null
+         */
         int x = currentLocation.x();
         int y = currentLocation.y();
         int[] xArr = {-1, 0, 1};
@@ -105,15 +120,30 @@ public class Koopa extends Actor{
 
     @Override
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        /**
+         * otherActor: Actor around the player
+         * direction: Direction in which otherActor is relative to Goomba
+         * map: current GameMap object
+         *
+         * returns: An ActionList object of actions that the other player can do to Koopa
+         */
         ActionList actions = new ActionList();
         // it can be attacked only by the HOSTILE opponent, and this action will not attack the HOSTILE enemy back.
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY) && this.getDisplayChar()!='D') {
             actions.add(new AttackAction(this,direction));
         }
         return actions;
     }
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        /**
+         * actions: ActionList of allowable actions of Koopa
+         * lastAction: Previous action of Koopa
+         * map: current GameMap
+         * display: current Display object
+         *
+         * returns: current action of Koopa. If none, returns DoNothingAction
+         */
         Location location = map.locationOf(this);
         tick(location);
         for(Behaviour Behaviour : behaviours.values()) {
