9119f34c1d9e1ef3abc9db125897fc8464be0647
Devureddy Prasanth Kumar Reddy <pdev0010@student.monash.edu>
2022-05-14 20:14:21 +1000

    continuation of LavaMap. need to teleport

--- src/game/Application.java
+++ src/game/Application.java
@@ -3,8 +3,10 @@ package game;
 import java.util.Arrays;
 import java.util.List;
 
+import edu.monash.fit2099.engine.actions.MoveActorAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
@@ -67,8 +69,22 @@ public class Application {
 
         world.addGameMap(gameMap);
         world.addGameMap(lavaZone);
+
         Actor mario = new Player("Player", 'm', 100);
-        world.addPlayer(mario, gameMap.at(42, 10));
+        world.addPlayer(mario, gameMap.at(0, 0));
+
+        WarpPipe warpPipe = new WarpPipe();
+        gameMap.at(0,0).addItem(new WarpPipe());
+        List<Item> itemsAtGround = gameMap.locationOf(mario).getItems();
+
+        itemsAtGround.forEach(item -> {
+            if(item.getDisplayChar() == 'c'){
+                System.out.println("Reached");
+                warpPipe.addSampleAction(new MoveActorAction(lavaZone.at(0,0), "to Lava Map"));
+            }
+        });
+
+
         // adding a toad character to the game
         gameMap.at( 45, 10).addActor(new Toad());
         // adding power star and super mushroom to the game
